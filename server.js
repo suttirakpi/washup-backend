61eeb0dcf6e3850ca37022daeb742a4e9fd29e68
Thatphon <thatphon.k@ku.th>
2026-03-07 22:01:33 +0700

    vehicles

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ app.use(express.json());
 mongoose.connect("mongodb://127.0.0.1:27017/carwashDB");
 
 const bcrypt = require("bcrypt"); // ใช้เข้ารหัสรหัสผ่าน ทำให้คนอื่นไม่สามารถเห็นรหัสผ่านจริงได้
-app.post("/users/register",async(req,res) => {
+const jwt = require("jsonwebtoken"); // ใช้สร้าง token สำหรับ authentication
+const SECRET = "mysecretkey"; // key สำหรับเข้ารหัส token
+
+// API สำหรับ Users (ผู้ใช้)//
+
+app.post("/users/register",async(req,res) => { //ใช้สำหรับลงทะเบียนผู้ใช้ใหม่ โดยรับข้อมูลจาก client ผ่าน HTTP POST และบันทึกลงใน MongoDB
     const db = mongoose.connection.collection("users");
 
     try{
@@ -16,6 +21,16 @@ app.post("/users/register",async(req,res) => {
     //เช็คข้อมูลครบไหมหรือไม่
     if(!req.body.login_name || !req.body.login_password || !req.body.fullname || !req.body.phone){
         return res.status(400).json({message:"please fill all fields"});
+    }
+
+    //ตรวจสอบความแข็งแรงของรหัสผ่าน
+    const password = req.body.login_password;
+    const strongPassword = /^(?=.*[A-Z])(?=.*[!@#$%]).{8,}$/;
+
+    if (!strongPassword.test(password)) {
+    return res.status(400).json({
+        message: "Password must be at least 8 characters, include 1 uppercase letter and 1 special character"
+    });
     }
 
     //เช็คว่ามี username ซ้ำไหม
@@ -58,7 +73,8 @@ app.post("/users/register",async(req,res) => {
       }
 
 });
-app.post("/users/login", async (req, res) => {
+
+app.post("/users/login", async (req, res) => { //ใช้สำหรับเข้าสู่ระบบผู้ใช้ โดยรับข้อมูลจาก client ผ่าน HTTP POST และตรวจสอบกับข้อมูลใน MongoDB
 
   const db = mongoose.connection.collection("users");
 
@@ -81,14 +97,23 @@ app.post("/users/login", async (req, res) => {
       req.body.login_password,
       user.login_password
     );
-
+    
     if (!isMatch) {
       return res.status(400).json({ message: "wrong password" });
     }
 
+      // สร้าง token
+    const token = jwt.sign(
+      { user_id: user.user_id },
+      SECRET,
+      { expiresIn: "1h" }
+    );
+
+
     // login สำเร็จ
     res.json({
       message: "login success",
+      token: token,
       user: {
         user_id: user.user_id,
         login_name: user.login_name,
@@ -100,9 +125,238 @@ app.post("/users/login", async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: "server error" });
   }
-
 });
-// เปิด Server
+
+function authMiddleware(req, res, next) {
+
+  const token = req.headers.authorization; // รับ token จาก header
+
+  if (!token) {
+    return res.status(401).json({ message: "no token" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, SECRET); // ตรวจ token
+    req.user = decoded; // เก็บ user_id ไว้ใน req
+    next(); // ไปทำ API ต่อ
+  } catch (error) {
+    res.status(401).json({ message: "invalid token" });
+  }
+}
+
+// API สำหรับดึงข้อมูลผู้ใช้ตาม user_id ทำส่วนหน้าโปรไฟล์ของผู้ใช้
+app.get("/users/:id", async (req, res) => {
+
+  // เชื่อม collection users ใน MongoDB
+  const db = mongoose.connection.collection("users");
+
+  try {
+
+    // รับค่า id จาก URL เช่น /users/1001
+    const userId = parseInt(req.params.id);
+
+    // ค้นหาผู้ใช้ใน database
+    const user = await db.findOne({ user_id: userId });
+
+    // ถ้าไม่พบผู้ใช้
+    if (!user) {
+      return res.status(404).json({
+        message: "user not found"
+      });
+    }
+
+    // ส่งข้อมูลกลับไปให้ frontend
+    res.json({
+      user_id: user.user_id,
+      login_name: user.login_name,
+      user_role: user.user_role,
+      fullname: user.fullname,
+      phone: user.phone
+    });
+
+  } catch (error) {
+
+    // ถ้า server error
+    res.status(500).json({
+      message: "server error"
+    });
+  }
+});
+
+// API สำหรับแก้ไขข้อมูลผู้ใช้ ทำส่วนหน้าโปรไฟล์ของผู้ใช้
+app.put("/users/:id", async (req, res) => {
+
+  // เชื่อม collection users
+  const db = mongoose.connection.collection("users");
+  try {
+    // รับ user_id จาก URL
+    const userId = parseInt(req.params.id);
+
+    // ค้นหาผู้ใช้ใน database
+    const { fullname, phone } = req.body;
+
+    // ตรวจว่ามีข้อมูลส่งมาหรือไม่
+    if (!fullname || !phone) {
+      return res.status(400).json({ message: "please fill fullname and phone" });
+    }
+
+     // อัปเดตข้อมูลใน database
+     const result = await db.updateOne(
+      { user_id: userId },
+      { $set: { fullname: fullname, phone: phone } }
+    );
+
+    // ถ้าไม่พบ user
+    if (result.matchedCount === 0) {
+      return res.status(404).json({
+        message: "user not found"
+      });
+    }
+
+    // ส่งข้อมูลกลับไปให้ frontend
+    res.json({
+      message: "user updated successfully",
+      
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: "server error" });
+  }
+});
+
+//API สำหรับ Vehicles (รถของลูกค้า)//
+
+// API สำหรับเพิ่มข้อมูลรถ หน้า Add Vehicle (เพิ่มรถ)
+app.post("/vehicles", authMiddleware, async (req, res) => {
+  // เชื่อม collection vehicles
+  const db = mongoose.connection.collection("vehicles");
+  try {
+    // รับข้อมูลจาก body
+    const {  vehicle_type_id, license_plate, brand, model, color, note } = req.body;
+
+    // ตรวจว่ามีข้อมูลส่งมาหรือไม่
+    if (!vehicle_type_id || !license_plate || !brand || !model || !color) {
+      return res.status(400).json({ message: "please fill all fields except note" });
+    }
+
+    // หา vehicle_id ล่าสุด
+    const lastVehicle = await db.find()
+      .sort({ vehicle_id: -1 })
+      .limit(1)
+      .toArray();
+
+    let newVehicleId = 2001; // ค่าเริ่มต้น
+
+    if (lastVehicle.length > 0) {
+      newVehicleId = lastVehicle[0].vehicle_id + 1;
+    }
+
+    // สร้าง object รถ
+    const vehicle = {
+      vehicle_id: newVehicleId,
+      user_id: req.user.user_id, // ใช้ user_id จาก token ที่ผ่าน authMiddleware มาแล้ว
+      vehicle_type_id: parseInt(vehicle_type_id),
+      license_plate,
+      brand,
+      model,
+      color,
+      note
+    };
+
+    //บันทึกข้อมูลลง MongoDB
+    const result = await db.insertOne(vehicle);
+
+    // ส่งข้อมูลกลับเป็น JSON
+    res.json({
+      message: "vehicle added successfully",
+      data: result
+    });
+  } catch (error) {
+    res.status(500).json({ message: "server error" });
+  }
+});
+
+// API สำหรับดึงรถทั้งหมดของ user (ใช้ token)
+app.get("/vehicles/user", authMiddleware, async (req, res) => {
+
+  const db = mongoose.connection.collection("vehicles");
+
+  try {
+
+    // ดึง user_id จาก token
+    const userId = req.user.user_id;
+
+    // ค้นหารถทั้งหมดของ user
+    const vehicles = await db.find({ user_id: userId }).toArray();
+
+    // ถ้าไม่มีรถ
+    if (vehicles.length === 0) {
+      return res.json({
+        message: "no vehicles found"
+      });
+    }
+
+    // ส่งข้อมูลกลับไป
+    res.json(vehicles);
+
+  } catch (error) {
+    res.status(500).json({
+      message: "server error"
+    });
+  }
+});
+
+// API สำหรับแก้ไขข้อมูลรถ
+app.put("/vehicles/:id", authMiddleware, async (req, res) => {
+
+  const db = mongoose.connection.collection("vehicles");
+
+  try{
+
+    // รับ vehicle_id จาก URL
+    const vehicleId = parseInt(req.params.id);
+
+    // รับข้อมูลใหม่จาก body
+    const { vehicle_type_id, license_plate, brand, model, color, note } = req.body;
+
+    // ตรวจว่ามีข้อมูลส่งมาหรือไม่
+    if (!vehicle_type_id || !license_plate || !brand || !model || !color) {
+      return res.status(400).json({ message: "please fill all fields except note" });
+    }
+
+    // อัปเดตข้อมูลรถ
+    const result = await db.updateOne(
+      { vehicle_id: vehicleId },
+      {
+        $set: {
+          vehicle_type_id: parseInt(vehicle_type_id),
+          license_plate: license_plate,
+          brand: brand,
+          model: model,
+          color: color,
+          note: note
+        }
+      }
+    );
+
+    // ถ้าไม่พบรถ
+    if (result.matchedCount === 0) {
+      return res.status(404).json({
+        message: "vehicle not found"
+      });
+    }
+    //ส่งผลลับกลับไปให้ frontend
+    res.json({
+      message: "vehicle updated successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "server error"
+    });
+  }
+});
+
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
